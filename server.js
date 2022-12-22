01d78e510053ff5318aeb1144f7b81f976436d88
Lussskki <monster.striker21@gmail.com>
2022-12-23 02:52:30 +0400

    methods of http 'get-post' for now

--- server.js
+++ server.js
@@ -1,43 +1,29 @@
 import express from 'express'
 import signale from 'signale'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+
 
 const app = express()
+      app.use(bodyParser.json())
 const port = 3000
 
 
-const users = [
-    {
-        id:1,
-        name:'Luka',
-        lastName:'Guledani'
-    },
-    {
-        id:2,
-        name:'Giorgi',
-        lastName:'Tchezia'
-    },
-    {
-        id:3,
-        name:'Luka',
-        lastName:'Gvaramia'
-    },
-    {
-        id:4,
-        name:'Irakli',
-        lastName:'Jishkariani'
-    }
-]
+const users = JSON.parse(fs.readFileSync('./db/myFriends.json').toString())
 
 
 
 
-app.get('/users', (req,res) =>{
+app.get('/users', (req,res,next) =>{
+    // console.log(users)
     res.send(users)
 })
 
 
-app.post('/users',()=>{
-    
+app.post('/users',(req,res,next)=>{
+    users.push(req.body)
+    fs.writeFileSync('./db/myFriends.json', JSON.stringify(users))
+    res.send('Post request recieved')
 })
 
 
