164a0492a145c848d6c974e081e4546cecf5548b
Lussskki <monster.striker21@gmail.com>
2022-12-23 17:51:25 +0400

    methods of http 'get-post'

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import fs from 'fs'
 
 
+
 const app = express()
       app.use(bodyParser.json())
 const port = 3000
@@ -21,11 +22,36 @@ app.get('/myFriends', (req,res,next) =>{
 
 
 app.post('/myFriends',(req,res,next)=>{
-    users.push(req.body)
-    fs.writeFileSync('./db/myFriends.json', JSON.stringify(users))
+    myFriends.push(req.body)
+    fs.writeFileSync('./db/myFriends.json', JSON.stringify(myFriends))
     res.send('Post request recieved')
 })
 
+app.put('/myFriends/:name',(req,res,next)=>{
+      const {name} = req.params
+      const {lastName} = req.body
+      let hasFound = false 
+      fs.writeFileSync('./db/myFriends.json', JSON.stringify(myFriends))
+
+
+      myFriends.forEach(i => {
+        if(name === name){
+            i.lastName = lastName,
+            hasFound = true
+        }
+        if(hasFound){
+            return res.json({
+                Message:'update successfuly'
+            })
+        }else{
+            return res.json({
+                Message:'user couldnt found'
+            })
+        }    
+ 
+
+      })
+})
 
 
 app.listen(port, (req,res)=>{
